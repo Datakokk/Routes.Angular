fe73419f78819857af941d3a6376c59722eb6fae
Datakokk <fiodorelzarster@gmx.com>
2021-03-29 11:30:43 +0200

    redirecting and wildcard routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';  
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,8 +22,9 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children: [
     { path: ':id', component:ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
-  ] }
-  
+  ] },
+  { path: 'not-found', component:PageNotFoundComponent},
+  { path: '**', redirectTo: '/not-found'}
 ]
 
 @NgModule({
@@ -33,7 +35,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
