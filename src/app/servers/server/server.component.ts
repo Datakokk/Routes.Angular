52488033f93917e4ea3383c847770779497ea6a3
Datakokk <fiodorelzarster@gmx.com>
2021-04-01 17:29:19 +0200

    resolving dynamic data whit the resolve guard

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -26,14 +26,21 @@ export class ServerComponent implements OnInit {
     // console.dir(this.numero);
     // this.server = this.serversService.getServer(this.numero);
 
-    const id = +this.route.snapshot.params['id'];
+    this.route.data
+        .subscribe(
+          (data: Data)=> this.server = data['server']
+        );
+
+    /*const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
 
     this.route.params.subscribe(
       (params: Params)=>{
           this.server = this.serversService.getServer(+params['id']);
       }
-    );
+    );*/
+
+
   }
 
   onEdit(){
